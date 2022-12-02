87e67f00c5ae95de7a428863275f245f5c4cd237
Raseek Sht <39650487+raseeksht@users.noreply.github.com>
2022-12-02 15:27:33 +0000

    download all images from all the pages

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class ZeroChan:
     def __init__(self,url,downloadLocation):
         self.url = url
         self.downloadLocation = os.path.abspath(downloadLocation)
+        self._curPage = 1
         self.threads = []
         
 
@@ -18,9 +19,11 @@ class ZeroChan:
             os.mkdir(self.downloadLocation)
     
     def fetch(self):
-        self._result = requests.get(self.url)
+        self._result = requests.get(self.url+f"&p={self._curPage}")
+        print(self.url+f"?p={self._curPage}")
         self._soup = BeautifulSoup(self._result.content,'html.parser')
 
+        
         self._totalPage = int(self._soup.find("p",{"class":"pagination"}).span.text.strip().split(" ")[-1])
         self._images = self._soup.find("ul",id="thumbs2").find_all("li")
 
@@ -29,16 +32,29 @@ class ZeroChan:
         for img in self._images:
             try:
                 mediaurl = img.div.a.img['src']
-                self.download(mediaurl,self.downloadLocation)
+                self.threads.append(threading.Thread(target=self.download,args=(mediaurl.split("/")[-1],self.downloadLocation,)))
+                # self.download(mediaurl.split("/")[-1],self.downloadLocation)
             except Exception:
                 print("error getting media url")
             
+        for thread in self.threads:
+            thread.start()
+        for thread in self.threads:
+            thread.join()
+        self.threads = []
+        if self._curPage<self._totalPage:
+            self._curPage +=1
+            print(self._curPage)
+            self.start()
 
+        
 
 
 
     def download(self,url,location):
-        os.system(f"wget -q --show-progress {url} -O {location}/{url.split('/')[-1]}")
+        self._hurl = "https://s1.zerochan.net/Genshin.Impact.600."
+        os.system(f"wget -q --show-progress {self._hurl}{url} -O {location}/{url.split('/')[-1]}")
+        # print(url)
 
 downloadLocation = "downloads"
 
