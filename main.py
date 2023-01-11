1cbaa0a35d6afdba2d9892723ee83b01e207b299
Raseek Sht <39650487+raseeksht@users.noreply.github.com>
2023-01-11 13:24:58 +0000

    user agent added while scraping and wget downoading

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ class ZeroChan:
         self.downloadLocation = os.path.abspath(downloadLocation)
         self._curPage = 1
         self.threads = []
+        self.headers = {
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
         
 
         # cretae dowload dir if not present
@@ -34,7 +37,7 @@ class ZeroChan:
             os.mkdir(self.downloadLocation)
     
     def fetch(self):
-        self._result = requests.get(self.url+f"&p={self._curPage}")
+        self._result = requests.get(self.url+f"&p={self._curPage}",headers=self.headers)
         print(self.url+f"?p={self._curPage}")
         self._soup = BeautifulSoup(self._result.content,'html.parser')
 
@@ -49,8 +52,8 @@ class ZeroChan:
                 mediaurl = img.div.a.img['src']
                 self.threads.append(threading.Thread(target=self.download,args=(mediaurl.split("/")[-1],self.downloadLocation,)))
                 # self.download(mediaurl.split("/")[-1],self.downloadLocation)
-            except Exception:
-                print("error getting media url")
+            except Exception as e:
+                print("error getting media url,may be registration required")
             
         for thread in self.threads:
             thread.start()
@@ -68,7 +71,7 @@ class ZeroChan:
 
     def download(self,url,location):
         self._hurl = "https://s1.zerochan.net/Genshin.Impact.600."
-        os.system(f"wget -q --show-progress {self._hurl}{url} -O {location}/{url.split('/')[-1]}")
+        os.system(f"""wget -U '{self.headers['User-Agent']}' -q --show-progress {self._hurl}{url} -O {location}/{url.split('/')[-1]}""")
         # print(url)
 
 with open("downloadLocation.txt") as f:
